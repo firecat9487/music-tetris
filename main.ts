d89f5931ff7099ffb229ab1255d646e18d00d666
firecat9487 <95409293+firecat9487@users.noreply.github.com>
2021-12-15 11:18:44 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,50 @@
-music.playTone(330, music.beat(BeatFraction.Double))
-music.playTone(247, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(294, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(247, music.beat(BeatFraction.Whole))
-music.playTone(220, music.beat(BeatFraction.Whole))
-music.playTone(220, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(330, music.beat(BeatFraction.Whole))
-music.playTone(294, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Double))
-music.playTone(247, music.beat(BeatFraction.Whole))
-music.playTone(247, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(294, music.beat(BeatFraction.Whole))
-music.playTone(330, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Half))
+input.onButtonPressed(Button.A, function () {
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+})
+for (let index = 0; index < 100; index++) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . # . .
+        # . . . #
+        . # . # .
+        `)
+}
